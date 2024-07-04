1b83a4a83a222a49a7edcf90ba3cbb5bb53848fb
zhouyang <602482790@qq.com>
2024-07-04 23:49:14 +0800

    提交一下

--- miniprogram/utils/CONSTANT.ts
+++ miniprogram/utils/CONSTANT.ts
@@ -3,7 +3,10 @@
 
 // const HOST = "localhost:80";
 // const HOST = "192.168.0.104:80";
-const HOST = "172.20.10.10:80";
+// const HOST = "172.20.10.10:80"; // Jiangxuehua iPhone
+
+const HOST = "172.20.10.12:80"; // DK iPhone
+
 
 export {
   HOST,
